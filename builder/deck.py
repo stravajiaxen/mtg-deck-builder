a6d3a57139bbd536dd80065ad2c9986f67a73fd4
Matthew DeMartino <demartino.mcd@gmail.com>
2022-06-08 19:53:24 -0400

    Set up package

---
+++ builder/deck.py
@@ -0,0 +1,15 @@
+cards = """
+1 Command Tower
+1 Daretti, Scrap Savant
+1 Exotic Orchard
+1 Ponder
+1 Illicit Shipment
+1 Marble Gargoyle
+1 Metropolis Angel
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
