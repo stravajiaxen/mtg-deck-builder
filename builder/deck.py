802f2b163c077adcb8bd65116014815515a52ab2
Matthew DeMartino <demartino.mcd@gmail.com>
2022-06-08 20:14:48 -0400

    Add ideas

--- builder/deck.py
+++ builder/deck.py
@@ -8,6 +8,19 @@ cards = """
 1 Metropolis Angel
 """
 
+ideas='''
+OK, so go to
+https://edhrec.com/decks/zaxara-the-exemplary
+and you'll see in the network a link to a json file like
+https://json.edhrec.com/v2/decks/zaxara-the-exemplary.json
+If you go into the json file you'll see a bunch of links to a bunch of decks
+that you can look at (see "table") with all the deets for the deck.
+
+Each deck preview is another json file like this
+https://json.edhrec.com/v2/deckpreview-temp/qPmdGyFT-CfpGLlLmpcS6A.json
+go to the json file and look at the cards. Voila! You now have a deck :D
+'''
+
 def main():
     pass
 
